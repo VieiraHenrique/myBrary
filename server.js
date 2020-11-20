746f11e12ae282c1ee7e53c397710ba6f411c97a
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-11-20 18:50:13 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+const { static } = require("express");
+const indexRouter = require("./routes/index");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+    "mongodb+srv://hvieira:hv454378@widodatabase.irnnf.mongodb.net/widoDataBase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to DB"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.set("layout", path.join(__dirname, "/layouts/layout"));
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server started on port 5000");
+});
