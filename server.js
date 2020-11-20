723a79de45d12930487624b64e544cdfd9eb5beb
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-11-20 19:30:44 +0100

    test

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const { static } = require("express");
 const indexRouter = require("./routes/index");
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(
+    "mongodb+srv://hvieira:hv454378@widodatabase.irnnf.mongodb.net/widoDataBase?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to DB"));
